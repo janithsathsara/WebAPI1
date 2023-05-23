ffde5cdc2b1a5cfac82dd385873394c10c34a73a
Janith Herath <Janith Herath@DESKTOP-T5PQ1NO>
2023-05-23 14:21:55 +0530

    Add project files.

---
+++ WebAPI1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using WebAPI1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI1.Controllers
+{
+
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa{ Id= 1, Name="Villa 1"},
+                new Villa{ Id= 1, Name="Villa 2"},
+            };
+        } 
+    }
+}
---
+++ WebAPI1/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI1.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
