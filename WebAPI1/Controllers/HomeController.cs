12f3dfa0804b1980e0ede3a097a0059513af8148
janithsathsara <53212744+janithsathsara@users.noreply.github.com>
2023-05-24 21:43:22 +0530

    24 may final changes

--- WebAPI1/Controllers/HomeController.cs
+++ WebAPI1/Controllers/HomeController.cs
@@ -79,7 +79,9 @@ namespace WebAPI1.Controllers
             VillaStore.villaList.Remove(villa);
             return NoContent();
         }
-        [HttpPut(("{id:int}", Name = "UpdateVilla"))]
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
         {
             if (villaDTO == null || id != villaDTO.Id)
@@ -88,6 +90,11 @@ namespace WebAPI1.Controllers
             }
 
             var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            villa.Name = villaDTO.Name;
+            villa.Sqft = villaDTO.Sqft;
+            villa.Occupancy = villaDTO.Occupancy;
+
+            return NoContent();
         }
 
     }
