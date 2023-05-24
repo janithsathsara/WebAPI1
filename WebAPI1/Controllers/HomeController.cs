d4ce63fa7245318ba15448dc44d06c9d9a6cc0d5
Janith Herath <Janith Herath@DESKTOP-T5PQ1NO>
2023-05-24 16:50:48 +0530

    office work

--- WebAPI1/Controllers/HomeController.cs
+++ WebAPI1/Controllers/HomeController.cs
@@ -10,16 +10,16 @@ namespace WebAPI1.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
-        public ActionResult <IEnumerable<VillaDTO>> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "getVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)] //[ProducesResponseType(StatusCodes.Status200OK), Type = typeof(VillaDTO)] can also be done with ActionResult type not being defined 
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult <VillaDTO> GetVilla(int id)
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
             if (id == 0)
             {
@@ -34,11 +34,20 @@ namespace WebAPI1.Controllers
         }
         [HttpPost]
 
-        [ProducesResponseType(StatusCodes.Status200OK)] 
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
+            //            if (!ModelState.IsValid) // in VillaDTO, the name field is Required. So, this gets triggered if name is ""
+            //            {
+            //                return BadRequest(ModelState);
+            //            }
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("Custom Error", "Villa already exists");//Custom error can be a number such as 400, but it should be something that doesn't exist
+                return BadRequest(ModelState);
+            }
             if (villaDTO == null)
             {
                 return BadRequest(villaDTO);
@@ -50,7 +59,36 @@ namespace WebAPI1.Controllers
             villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
             VillaStore.villaList.Add(villaDTO);
 
-            return Ok(villaDTO);
+            return CreatedAtRoute("getVilla", new { id = villaDTO.Id }, villaDTO);
         }
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+        }
+        [HttpPut(("{id:int}", Name = "UpdateVilla"))]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+        {
+            if (villaDTO == null || id != villaDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
+
     }
 }
--- WebAPI1/Data/VillaStore.cs
+++ WebAPI1/Data/VillaStore.cs
@@ -6,8 +6,8 @@ namespace WebAPI1.Data
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>
             {
-                new VillaDTO{ Id= 1, Name="Villa 1"},
-                new VillaDTO{ Id= 2, Name="Villa 2"},
+                new VillaDTO{ Id= 1, Name="Villa 1", Occupancy=100, Sqft = 4 },
+                new VillaDTO{ Id= 2, Name="Villa 2", Occupancy=100, Sqft = 3},
             };
     }
 }
